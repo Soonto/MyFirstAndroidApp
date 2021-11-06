d30aa8ac86ebb59a1945350b99f9424800faaf40
Pavel Meshshikhin <SoontoGit@gmail.com>
2021-11-06 05:02:40 +0300

    Две рабочие кнопки

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.myfirstapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textFild)
+        val okButton = findViewById<View>(R.id.okButton)
+        val cancelButton = findViewById<View>(R.id.canselButton)
+        okButton.setOnClickListener { textView.text = """Push "ok" button""" }
+        cancelButton.setOnClickListener { textView.text = """Push "cancel" button""" }
     }
-    fun send_Message(view : View){
-        val message = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
-        startActivity(Intent(this, DisplayMessageActiviti::class.java).apply{putExtra(EXTRA_MESSAGE, message)})
-    }
+
+
 }
